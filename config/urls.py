0d0ed6f2880fd8ce735650e3803f4379ef75bb23
ibragim-Nalgiev <ibragim.nalgieff@yandex.ru>
2023-10-03 21:39:55 +0300

    added static files

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for educational_module project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('users/', include('users.urls', namespace='users')),
+    path('educational_modules/', include('educational_modules.urls', namespace='educational_modules'))
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
