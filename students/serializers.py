a3ddd3bc9e2585d02ff52c0a8ab4b4ce0034c7b1
ibragim-Nalgiev <ibragim.nalgieff@yandex.ru>
2023-10-09 09:44:28 +0300

    validator is ready

--- students/serializers.py
+++ students/serializers.py
@@ -2,6 +2,8 @@ from students.models import Student
 
 from rest_framework import serializers
 
+from students.validators import FullnameValidator
+
 
 class StudentSerializer(serializers.ModelSerializer):
     """ Класс сериализатор для студента """
@@ -9,6 +11,7 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ("id", "first_name", "last_name", "avatar", "email", "phone", "is_active", "modul",)
+        validators = [FullnameValidator(fields=("first_name", "last_name"))]
 
 
 class StudentSmallSerializer(serializers.ModelSerializer):
